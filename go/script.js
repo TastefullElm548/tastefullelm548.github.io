f2bff1d895be8a0f838e8553a02eedc88fd4db23
TastefullElm548 <92990534+TastefullElm548@users.noreply.github.com>
2023-01-26 21:47:38 +0100

    Fix empty cookie

--- go/script.js
+++ go/script.js
@@ -28,10 +28,12 @@ function getCookie(cname) {
 }
 
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    if (cvalue == "https://tastefullelm548.github.io/go") {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
 }
 
 function getParameters() {
